38fc8b065e296662b9d0fceed17ddcc9372e834a
matheus-liber <matheuslibergt@gmail.com>
2022-12-30 15:57:24 -0300

    finalização do projeto

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,8 +1,14 @@
+const elementoParaInserirLivros = document.getElementById("livros");
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById("valor_total_livros_disponiveis");
+
 function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = "";
+    elementoParaInserirLivros.innerHTML = "";
     listaDeLivros.forEach(livro => {
+        let disponibilidade = livro.quantidade > 0 ? "livro__imagens" : "livros__imagens indisponivel";
         elementoParaInserirLivros.innerHTML += `
             <div class="livro">
-                <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+                <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
                 <h2 class="livro__titulo">
                     ${livro.titulo}
                 </h2>
@@ -15,3 +21,4 @@ function exibirOsLivrosNaTela(listaDeLivros) {
         `
     })
 }
+
